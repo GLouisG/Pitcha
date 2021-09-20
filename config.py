1f23aeb7ed8a14830e3ca3435f6b4294ee8fee27
GLouisG <louis.gacho@student.moringaschool.com>
2021-09-20 19:39:08 +0300

    attempt at fixing deployment1

--- config.py
+++ config.py
@@ -19,6 +19,7 @@ class ProdConfig(Config) :
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
         SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+        
     '''
     Production  configuration child class of ABOVE
 
